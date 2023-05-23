272317a933c8861f486fc47d284ef07dbf9f8178
Ermal-code <ermal.aa@live.com>
2023-05-23 16:09:59 +0200

    Added express-session

--- src/server.js
+++ src/server.js
@@ -1,65 +1,65 @@
-const express = require("express");
-const listEndpoints = require("express-list-endpoints");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-const passport = require("passport");
-const cookieParser = require("cookie-parser");
+const express = require("express")
+const listEndpoints = require("express-list-endpoints")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const helmet = require("helmet")
+const passport = require("passport")
+const cookieParser = require("cookie-parser")
 
-const usersRoute = require("./services/users");
-const weatherApiRoute = require("./services/weatherApi");
-const oauth = require("./utils/auth/oAuth");
+const usersRoute = require("./services/users")
+const weatherApiRoute = require("./services/weatherApi")
+const oauth = require("./utils/auth/oAuth")
 
 const {
-  notFoundErrorHandler,
-  unauthorizedErrorHandler,
-  badRequestErrorHandler,
-  forbiddenErrorHandler,
-  catchAllErrorHandler,
-} = require("./errorHandling");
+    notFoundErrorHandler,
+    unauthorizedErrorHandler,
+    badRequestErrorHandler,
+    forbiddenErrorHandler,
+    catchAllErrorHandler,
+} = require("./errorHandling")
 
-const server = express();
+const server = express()
 
-server.set("trust proxy", 1);
-server.enable("trust proxy");
+server.set("trust proxy", 1)
+server.enable("trust proxy")
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3003
 
-const whitelist = [`${process.env.FE_URL}`];
+const whitelist = [`${process.env.FE_URL}`]
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-server.use(cors(corsOptions));
-server.use(helmet());
-server.use(express.json());
-server.use(cookieParser());
-server.use(passport.initialize());
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+}
+server.use(cors(corsOptions))
+server.use(helmet())
+server.use(express.json())
+server.use(cookieParser())
+server.use(passport.initialize())
 
-server.use("/users", usersRoute);
-server.use("/weather", weatherApiRoute);
+server.use("/users", usersRoute)
+server.use("/weather", weatherApiRoute)
 
-server.use(badRequestErrorHandler);
-server.use(notFoundErrorHandler);
-server.use(forbiddenErrorHandler);
-server.use(unauthorizedErrorHandler);
-server.use(catchAllErrorHandler);
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(unauthorizedErrorHandler)
+server.use(catchAllErrorHandler)
 
-console.log(listEndpoints(server));
+console.log(listEndpoints(server))
 
-mongoose.set("debug", true);
+mongoose.set("debug", true)
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(server.listen(port, () => console.log("Running on port", port)))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(server.listen(port, () => console.log("Running on port", port)))
+    .catch((err) => console.log(err))
