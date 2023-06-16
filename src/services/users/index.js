62b9df38da050f5aef361f8935448c19740a5f67
Ermal-code <ermal.aa@live.com>
2023-06-16 15:33:21 +0200

    minor fixes

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,269 +1,252 @@
-const router = require("express").Router();
-const UserModel = require("./schema");
-const { authenticateUser, refreshToken } = require("../../utils/auth");
-const {
-  authorizeUser,
-  adminOnly,
-} = require("../../utils/auth/authMiddlewares");
-const passport = require("passport");
-const multer = require("multer");
-const cloudinary = require("../../utils/cloudinaryConfig");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const router = require("express").Router()
+const UserModel = require("./schema")
+const { authenticateUser, refreshToken } = require("../../utils/auth")
+const { authorizeUser, adminOnly } = require("../../utils/auth/authMiddlewares")
+const passport = require("passport")
+const multer = require("multer")
+const cloudinary = require("../../utils/cloudinaryConfig")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
 
 router.get("/", authorizeUser, adminOnly, async (req, res, next) => {
-  try {
-    const users = await UserModel.find();
-    res.status(200).send(users);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+    try {
+        const users = await UserModel.find()
+        res.status(200).send(users)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.get("/me", authorizeUser, async (req, res, next) => {
-  try {
-    res.status(200).send(req.user);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+    try {
+        res.status(200).send(req.user)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/register", async (req, res, next) => {
-  try {
-    const newUser = new UserModel(req.body);
-    const { _id } = await newUser.save();
+    try {
+        const newUser = new UserModel(req.body)
+        const { _id } = await newUser.save()
 
-    const { accessToken, refreshToken } = await authenticateUser(newUser);
+        const { accessToken, refreshToken } = await authenticateUser(newUser)
 
-    res.cookie("accessToken", accessToken, {
-      httpOnly: true,
-      path: "/",
-      secure: true,
-      sameSite: "none",
-    });
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            path: "/",
+            secure: true,
+            sameSite: "none",
+        })
 
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      path: "/users/refreshToken",
-      secure: true,
-      sameSite: "none",
-    });
-    res.status(201).send(_id);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            path: "/users/refreshToken",
+            secure: true,
+            sameSite: "none",
+        })
+        res.status(201).send(_id)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.put("/me", authorizeUser, async (req, res, next) => {
-  try {
-    const updates = Object.keys(req.body);
-    updates.forEach((update) => (req.user[update] = req.body[update]));
-    await req.user.save();
+    try {
+        const updates = Object.keys(req.body)
+        updates.forEach((update) => (req.user[update] = req.body[update]))
+        await req.user.save()
 
-    res.status(200).send(req.user);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        res.status(200).send(req.user)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.delete("/me", authorizeUser, async (req, res, next) => {
-  try {
-    await req.user.deleteOne();
+    try {
+        await req.user.deleteOne()
 
-    res.status(203).send("User deleted");
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        res.status(203).send("User deleted")
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/addToFav", authorizeUser, async (req, res, next) => {
-  try {
-    const user = req.user;
+    try {
+        const user = req.user
 
-    await user.updateOne({ $addToSet: { favorites: req.body.favorite } });
+        await user.updateOne({ $addToSet: { favorites: req.body.favorite } })
 
-    res.status(200).send(user);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        res.status(200).send(user)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/removeFromFav", authorizeUser, async (req, res, next) => {
-  try {
-    const user = req.user;
+    try {
+        const user = req.user
 
-    await user.updateOne({ $pull: { favorites: req.body.favorite } });
+        await user.updateOne({ $pull: { favorites: req.body.favorite } })
 
-    res.status(200).send(user);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        res.status(200).send(user)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body
 
-    const user = await UserModel.findByCredentials(email, password);
-    if (user) {
-      const { accessToken, refreshToken } = await authenticateUser(user);
+        const user = await UserModel.findByCredentials(email, password)
+        if (user) {
+            const { accessToken, refreshToken } = await authenticateUser(user)
 
-      res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        path: "/",
-        secure: true,
-        sameSite: "none",
-      });
+            res.cookie("accessToken", accessToken, {
+                httpOnly: true,
+                path: "/",
+                secure: true,
+                sameSite: "none",
+            })
 
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        path: "/users/refreshToken",
-        secure: true,
-        sameSite: "none",
-      });
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                path: "/users/refreshToken",
+                secure: true,
+                sameSite: "none",
+            })
 
-      res.status(200).send(user);
-    } else {
-      const err = new Error();
-      err.message = `Email or password is wrong`;
-      err.httpStatusCode = 404;
-      next(err);
+            res.status(200).send(user)
+        } else {
+            const err = new Error()
+            err.message = `Email or password is wrong`
+            err.httpStatusCode = 404
+            next(err)
+        }
+    } catch (error) {
+        console.log(error)
+        next(error)
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+})
 
 router.post("/logout", authorizeUser, async (req, res, next) => {
-  try {
-    newRefreshTokens = req.user.refreshTokens.filter(
-      (token) => token.refreshToken !== req.token.refreshToken
-    );
-    await req.user.updateOne({ refreshTokens: newRefreshTokens });
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
-    res.send("ok");
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+    try {
+        const newRefreshTokens = req.user.refreshTokens.filter((token) => token.refreshToken !== req.token.refreshToken)
+        await req.user.updateOne({ refreshTokens: newRefreshTokens })
+        res.clearCookie("accessToken")
+        res.clearCookie("refreshToken")
+        res.send("ok")
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/logoutAll", authorizeUser, async (req, res, next) => {
-  try {
-    req.user.refreshTokens = [];
+    try {
+        req.user.refreshTokens = []
 
-    await req.user.save();
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
-    res.send();
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+        await req.user.save()
+        res.clearCookie("accessToken")
+        res.clearCookie("refreshToken")
+        res.send()
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 router.post("/refreshToken", async (req, res, next) => {
-  const oldRefreshToken = req.cookies.refreshToken;
+    const oldRefreshToken = req.cookies.refreshToken
 
-  if (!oldRefreshToken) {
-    const err = new Error("Refresh token is missing");
-    err.httpStatusCode = 400;
-    next(err);
-  } else {
-    try {
-      const tokens = await refreshToken(oldRefreshToken);
+    if (!oldRefreshToken) {
+        const err = new Error()
+        err.message = "Refresh token is missing"
+        err.httpStatusCode = 400
+        next(err)
+    } else {
+        try {
+            const tokens = await refreshToken(oldRefreshToken)
 
-      res.cookie("accessToken", tokens.accessToken, {
-        httpOnly: true,
-        path: "/",
-        secure: true,
-        sameSite: "none",
-      });
+            res.cookie("accessToken", tokens.accessToken, {
+                httpOnly: true,
+                path: "/",
+                secure: true,
+                sameSite: "none",
+            })
 
-      res.cookie("refreshToken", tokens.refreshToken, {
-        httpOnly: true,
-        path: "/users/refreshToken",
-        secure: true,
-        sameSite: "none",
-      });
+            res.cookie("refreshToken", tokens.refreshToken, {
+                httpOnly: true,
+                path: "/users/refreshToken",
+                secure: true,
+                sameSite: "none",
+            })
 
-      res.send("OK");
-    } catch (error) {
-      console.log(error);
-      const err = new Error(error);
-      err.httpStatusCode = 403;
-      next(err);
+            res.send("OK")
+        } catch (error) {
+            const err = new Error(error)
+            err.httpStatusCode = 403
+            next(err)
+        }
     }
-  }
-});
+})
 
-router.get(
-  "/googleLogin",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+router.get("/googleLogin", passport.authenticate("google", { scope: ["profile", "email"] }))
 
-router.get(
-  "/googleRedirect",
-  passport.authenticate("google"),
-  async (req, res, next) => {
+router.get("/googleRedirect", passport.authenticate("google"), async (req, res, next) => {
     try {
-      res.cookie("accessToken", req.user.tokens.accessToken, {
-        httpOnly: true,
-        path: "/",
-        secure: true,
-        sameSite: "none",
-      });
+        res.cookie("accessToken", req.user.tokens.accessToken, {
+            httpOnly: true,
+            path: "/",
+            secure: true,
+            sameSite: "none",
+        })
 
-      res.cookie("refreshToken", req.user.tokens.refreshToken, {
-        httpOnly: true,
-        path: "/users/refreshToken",
-        secure: true,
-        sameSite: "none",
-      });
+        res.cookie("refreshToken", req.user.tokens.refreshToken, {
+            httpOnly: true,
+            path: "/users/refreshToken",
+            secure: true,
+            sameSite: "none",
+        })
 
-      res.status(200).redirect(`${process.env.FE_URL}/home`);
+        res.status(200).redirect(`${process.env.FE_URL}/home`)
     } catch (error) {
-      console.log(error);
-      next(error);
+        console.log(error)
+        next(error)
     }
-  }
-);
+})
 
 const cloudStorage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "profile",
-  },
-});
+    cloudinary: cloudinary,
+    params: {
+        folder: "profile",
+    },
+})
 
-const cloudMulter = multer({ storage: cloudStorage });
+const cloudMulter = multer({ storage: cloudStorage })
 
-router.post(
-  "/uploadPicture",
-  authorizeUser,
-  cloudMulter.single("picture"),
-  async (req, res, next) => {
+router.post("/uploadPicture", authorizeUser, cloudMulter.single("picture"), async (req, res, next) => {
     try {
-      const user = req.user;
-      await user.updateOne({
-        $set: {
-          img: req.file.path,
-        },
-      });
-      res.status(201).send(user);
+        const user = req.user
+        await user.updateOne({
+            $set: {
+                img: req.file.path,
+            },
+        })
+        res.status(201).send(user)
     } catch (error) {
-      console.log(error);
-      next(error);
+        console.log(error)
+        next(error)
     }
-  }
-);
+})
 
-module.exports = router;
+module.exports = router
--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -1,81 +1,61 @@
-const jwt = require("jsonwebtoken");
-const UserModel = require("../../services/users/schema");
+const jwt = require("jsonwebtoken")
+const UserModel = require("../../services/users/schema")
 
 const authenticateUser = async (user) => {
-  try {
-    const newAccessToken = await generateJWT(
-      { _id: user._id },
-      process.env.JWT_SECRET
-    );
-    const newRefreshToken = await generateJWT(
-      { _id: user._id },
-      process.env.RERFRESH_JWT_SECRET
-    );
+    try {
+        const newAccessToken = await generateJWT({ _id: user._id }, process.env.JWT_SECRET)
+        const newRefreshToken = await generateJWT({ _id: user._id }, process.env.RERFRESH_JWT_SECRET)
 
-    user.refreshTokens = [...user.refreshTokens, newRefreshToken];
-    await user.save();
+        user.refreshTokens = [...user.refreshTokens, newRefreshToken]
+        await user.save()
 
-    return { accessToken: newAccessToken, refreshToken: newRefreshToken };
-  } catch (error) {
-    console.log(error);
-  }
-};
+        return { accessToken: newAccessToken, refreshToken: newRefreshToken }
+    } catch (error) {
+        console.log({ error })
+    }
+}
 
 const generateJWT = (payload, secret) =>
-  new Promise((res, rej) =>
-    jwt.sign(
-      payload,
-      secret,
-      { expiresIn: secret === process.env.JWT_SECRET ? "15m" : "1 week" },
-      (err, token) => {
-        if (err) rej(err);
-        res(token);
-      }
+    new Promise((res, rej) =>
+        jwt.sign(payload, secret, { expiresIn: secret === process.env.JWT_SECRET ? "15m" : "1 week" }, (err, token) => {
+            if (err) rej(err)
+            res(token)
+        })
     )
-  );
 
 const verifyJWT = (token, secret) =>
-  new Promise((res, rej) =>
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) rej(err);
-      res(decoded);
-    })
-  );
+    new Promise((res, rej) =>
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) rej(err)
+            res(decoded)
+        })
+    )
 
 const refreshToken = async (oldRefreshToken) => {
-  const decoded = await verifyJWT(
-    oldRefreshToken,
-    process.env.RERFRESH_JWT_SECRET
-  );
-  const user = await UserModel.findOne({ _id: decoded._id });
-  if (!user) {
-    throw new Error("Access to this action is forbidden for this user");
-  }
+    const decoded = await verifyJWT(oldRefreshToken, process.env.RERFRESH_JWT_SECRET)
+    const user = await UserModel.findOne({ _id: decoded._id })
+    if (!user) {
+        const err = new Error()
+        err.message = "Access to this action is forbidden for this user"
+        throw err
+    }
 
-  const currentRefreshToken = user.refreshTokens.find(
-    (token) => token === oldRefreshToken
-  );
+    const currentRefreshToken = user.refreshTokens.find((token) => token === oldRefreshToken)
 
-  if (!currentRefreshToken) {
-    throw new Error("Refresh token is wrong");
-  }
+    if (!currentRefreshToken) {
+        const err = new Error()
+        err.message = "Refresh token is wrong"
+        throw err
+    }
 
-  const newAccessToken = await generateJWT(
-    { _id: user._id },
-    process.env.JWT_SECRET
-  );
-  const newRefreshToken = await generateJWT(
-    { _id: user._id },
-    process.env.RERFRESH_JWT_SECRET
-  );
+    const newAccessToken = await generateJWT({ _id: user._id }, process.env.JWT_SECRET)
+    const newRefreshToken = await generateJWT({ _id: user._id }, process.env.RERFRESH_JWT_SECRET)
 
-  user.refreshTokens[
-    user.refreshTokens.indexOf(currentRefreshToken)
-  ] = newRefreshToken;
+    user.refreshTokens[user.refreshTokens.indexOf(currentRefreshToken)] = newRefreshToken
 
-  await user.updateOne({ refreshTokens: user.refreshTokens });
+    await user.updateOne({ refreshTokens: user.refreshTokens })
 
-  return { accessToken: newAccessToken, refreshToken: newRefreshToken };
-};
+    return { accessToken: newAccessToken, refreshToken: newRefreshToken }
+}
 
-module.exports = { authenticateUser, verifyJWT, refreshToken };
+module.exports = { authenticateUser, verifyJWT, refreshToken }
